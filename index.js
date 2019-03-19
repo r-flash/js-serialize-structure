2857739a56e00a6943a1a1ac4f5791217b259f8a
Adam Platkevič <rflashster@gmail.com>
2019-03-19 18:52:35 +0100

    add basic support for symbols

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
 const VALUE_NAN = -1;
 const VALUE_INFTY = -2;
 const VALUE_NEG_INFTY = -3;
+const VALUE_UNDEFINED = -4;
+
+function isStrictlyNaN(val) {
+    return typeof val === "number" && isNaN(val);
+}
 
 exports.serialize = function serialize(obj) {
     const indexMap = new Map();
     const output = [];
 
     function getIndex(val) {
-        if(indexMap.has(val)) return indexMap.get(val);
+        // return index if the value has been already seen
+        if (indexMap.has(val)) return indexMap.get(val);
 
-        if(typeof val === "number" && isNaN(val)) return VALUE_NAN;
+        // handle non-JSON.stringify-able primitive values
+        if (typeof val === "symbol") return "_" + (Symbol.keyFor(val) || "");
+        if (isStrictlyNaN(val)) return VALUE_NAN;
+        if (val === Infinity) return VALUE_INFTY;
+        if (val === -Infinity) return VALUE_NEG_INFTY;
+        if (val === undefined) return VALUE_UNDEFINED;
 
-        if(val === Infinity) return VALUE_INFTY;
-
-        if(val === -Infinity) return VALUE_NEG_INFTY;
-
+        // memoize the index of this value
         const index = output.length;
         indexMap.set(val, index);
 
-        if(typeof val !== "object" || val === null) output.push(val);
-        else {
+        if (typeof val !== "object" || val === null) {
+            // save primitives verbatim
+            output.push(val);
+        } else {
+            // save objects as dictionaries of indices
             const dict = {};
             output.push(dict);
-            const keys = Object.keys(val);
-            for (let i = 0; i < keys.length; i++) {
-                const key = keys[i];
-                dict[key] = getIndex(val[key]);
+
+            // recursively add all object's members
+            for (const key in val) {
+                if (val.hasOwnProperty(key)) {
+                    dict[key] = getIndex(val[key]);
+                }
             }
         }
         return index;
     }
 
+    // start with the root object
     getIndex(obj);
 
     return JSON.stringify(output);
@@ -41,32 +55,34 @@ exports.deserialize = function deserialize(str) {
     const valueMap = new Map();
 
     function getValue(index) {
-        if(valueMap.has(index)) return valueMap.get(index);
+        // return value for this index if already constructed
+        if (valueMap.has(index)) return valueMap.get(index);
 
-        if(index === VALUE_NAN) return NaN;
-
-        if(index === VALUE_INFTY) return Infinity;
-
-        if(index === VALUE_NEG_INFTY) return -Infinity;
+        // handle pseudo-indices of non-JSON.stringify-able primitive values
+        if (typeof index === "string") return index.length > 1 ? Symbol.for(index.substr(1)) : Symbol();
+        if (index === VALUE_NAN) return NaN;
+        if (index === VALUE_INFTY) return Infinity;
+        if (index === VALUE_NEG_INFTY) return -Infinity;
+        if (index === VALUE_UNDEFINED) return undefined;
 
         const dict = input[index];
 
-        if(typeof dict !== "object" || dict === null) {
+        // handle primitive values
+        if (typeof dict !== "object" || dict === null) {
             valueMap.set(index, dict);
             return dict;
         }
 
+        // handle index distionaries
         const val = {};
         valueMap.set(index, val);
 
-        const keys = Object.keys(dict);
-        for (let i = 0; i < keys.length; i++) {
-            const key = keys[i];
-            val[key] = getValue(dict[key]);
-        }
+        // populate the object by recursively constructing its members
+        for (const key in dict) val[key] = getValue(dict[key]);
 
         return val;
     }
 
+    // start with the first index
     return getValue(0);
 };
---
+++ test/index.js
@@ -0,0 +1,116 @@
+const assert = require("assert");
+const Ser = require("../index");
+
+it('should deserialize simple object', function () {
+    const obj = { a: "a", b: "b" };
+
+    const sized = Ser.serialize(obj);
+    const dized = Ser.deserialize(sized);
+
+    assert.deepStrictEqual(dized, obj);
+});
+
+it('should deserialize complex object', function () {
+    const obj = {
+        a: "a",
+        b: "b",
+        0: 0,
+        1: 1,
+        2: 2,
+        pi: Math.PI,
+        " ": " ",
+        "\0": "\0",
+        "]": "]",
+        obj1: {
+            i_am: "nested",
+            obj1_1: {},
+            obj1_2: {},
+        },
+        obj2: {
+            i_am: "nested too",
+            obj2_1: {},
+            obj2_2: {}
+        }
+    };
+
+    const sized = Ser.serialize(obj);
+    const dized = Ser.deserialize(sized);
+
+    assert.deepStrictEqual(dized, obj);
+});
+
+it('should preserve reference equality', function () {
+    const nested = {};
+
+    const sized = Ser.serialize({ a: nested, b: nested });
+    const dized = Ser.deserialize(sized);
+
+    assert.strictEqual(dized.a, dized.b);
+});
+
+it('should handle circular references', function () {
+    const obj = {};
+    obj.obj = obj;
+    obj.nested = { obj };
+    obj.nested.nested = obj.nested;
+
+    const sized = Ser.serialize(obj);
+    const dized = Ser.deserialize(sized);
+
+    assert.strictEqual(dized.obj, dized);
+    assert.strictEqual(dized.nested.obj, dized);
+    assert.strictEqual(dized.nested.nested,  dized.nested);
+});
+
+it('should handle NaN, Infinity and undefined', function () {
+    const obj = {
+        nan: NaN,
+        infty: Infinity,
+        negInfty: -Infinity,
+        undefined: undefined
+    };
+
+    const sized = Ser.serialize(obj);
+    const dized = Ser.deserialize(sized);
+
+    assert.deepStrictEqual(dized, obj);
+});
+
+it('should handle symbols as values', function () {
+    const obj = { s1: Symbol(), s2: Symbol() };
+
+    const sized = Ser.serialize(obj);
+    const dized = Ser.deserialize(sized);
+
+    assert.strictEqual(typeof dized.s1, "symbol");
+    assert.strictEqual(typeof dized.s2, "symbol");
+    assert.notStrictEqual(dized.s1, dized.s2);
+});
+
+it('should preserve symbol equality', function () {
+    const symbol = Symbol();
+    const obj = { s1: symbol, s2: symbol };
+
+    const sized = Ser.serialize(obj);
+    const dized = Ser.deserialize(sized);
+
+    console.log(sized);
+
+    assert.strictEqual(dized.s1, dized.s2);
+});
+
+it('should handle keyed symbols as values', function () {
+    const obj = {
+        s1: Symbol(),
+        s2: Symbol.for("key"),
+        s3: Symbol.for("key")
+    };
+
+    const sized = Ser.serialize(obj);
+    const dized = Ser.deserialize(sized);
+
+    assert.notStrictEqual(dized.s1, dized.s2);
+    assert.strictEqual(dized.s2, dized.s3);
+    assert(!Symbol.keyFor(dized.s1));
+    assert.strictEqual(Symbol.keyFor(dized.s2), "key");
+});
